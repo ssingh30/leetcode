a6a8596a17a2e1860b8b981f2c9fe71bec46ee6b
Saumya Singh <saumysin@cisco.com>
2020-08-10 19:57:56 -0700

    fixed

--- shuffleString.js
+++ shuffleString.js
@@ -1,7 +1,10 @@
 const shuffle=(s, indices)=>{
 	let newString = "";
-	for(let i=0; i<=s.length; i++){
+	for(let i=0; i<s.length; i++){
+		let index = indices.indexOf(i);
 		let char = s[index];
 		newString += char;	
 	}
+	return newString;
 }
+
